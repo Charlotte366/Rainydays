6fc4a6e4e8c2c95f1af0863807c4fa747be30419
Charlotte366 <jc.oren@hotmail.com>
2021-03-21 22:22:16 +0100

    update

--- js/checkout.js
+++ js/checkout.js
@@ -1,30 +1,32 @@
 const form = document.querySelector("#contactForm");
-const name = document.querySelector("#name");
-const nameError = document.querySelector("#nameError");
-//const lastName = document.querySelector("#lastName");
-//const lastNameError = document.querySelector("lastNameError")
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
 const city = document.querySelector("#city");
 const cityError = document.querySelector("#cityError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
 const adress = document.querySelector("#adress");
 const adressError = document.querySelector("#adressError");
-const cardNumber = document.querySelector("#cardNumber");
-const cardNumberError = document.querySelector("#cardNumberError");
-const cardholderName = document.querySelector("#cardholderName")
-const cardholderNameError = document.querySelector("#cardholderNameError")
+const cardnumber = document.querySelector("#cardnumber");
+const cardnumberError = document.querySelector("#cardnumberError");
+const cardholdername = document.querySelector("#cardholdername");
+const cardholdernameError = document.querySelector("#cardholdernameError");
+const expiredate = document.querySelector("#expiredate");
+const expiredateError = document.querySelector("#expiredateError");
 const cvv = document.querySelector("#cvv");
-const cvvError = document.querySelector("cvvError");
+const cvvError = document.querySelector("#cvvError");
 
-function validateForm(){
+
+function validateForm() {
     event.preventDefault();
 
-    if (name.value.trim().length > 0) {
-        nameError.style.display = "none";
-    } else {
-        nameError.style.display ="block";
+    if(firstName.value.length > 0) {
+        firstNameError.style.display = "none";
     }
+    else {
+        firstNameError.style.display = "block";
 
+    }
     if (lastName.value.trim().length > 0) {
         lastNameError.style.display = "none";
     } else {
@@ -43,18 +45,6 @@ function validateForm(){
         emailError.style.display ="block";
     }
 
-    if (validateCardNumber(cardNumber.value) === true) {
-        cardNumberError.style.display = "none";
-    } else {
-        cardNumberError.style.display ="block";
-    }
-
-    if (validateCvv(cvv.value) === true) {
-        cvvError.style.display = "none";
-    } else {
-        cvvError.style.display ="block";
-    }
-    
 
     if (adress.value.trim().length > 9) {
         adressError.style.display = "none";
@@ -74,12 +64,43 @@ function validateForm(){
         countryError.style.display ="block";
     }
 
+    if (validatecardnumber(cardnumber.value) === true) {
+        cardnumberError.style.display = "none";
+    } else {
+        cardnumberError.style.display ="block";
+    }
+
+    if(cardholdername.value.length > 0) {
+        cardholdernameError.style.display = "none";
+    }
+    else {
+        cardholdernameError.style.display = "block";
+
+    }
+
+    if (validateexpiredate(expiredate.value) === true) {
+        expiredateError.style.display = "none";
+    } else {
+        expiredateError.style.display ="block";
+    }
+
+    if (validatecvv(cvv.value) === true) {
+        cvvError.style.display = "none";
+    } else {
+        cvvError.style.display ="block";
+    }
+    
+
+    
 
 
-    console.log("Hey");
 }
 
+console.log("hey");
+
 form.addEventListener("submit", validateForm);
+
+
 
 function validateEmail(email) {
     const regEx = /\S+@\S+\.\S+/;
@@ -87,37 +108,24 @@ function validateEmail(email) {
     return patternMatches;
 }
 
-function validateCardNumber(cardNumber) {
-    const regEx =  /^\d+$/;
-    const patternMatches = regEx.test(cardNumber);
+function validatecardnumber(cardnumber) {
+    const regEx = /^\d+$/;
+    const patternMatches = regEx.test(cardnumber);
     return patternMatches;
 }
 
-function validateCvv(cvv) {
-    const regEx =  /^\d+$/;
+function validateexpiredate(expiredate) {
+    const regEx = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/
+    const patternMatches = regEx.test(expiredate);
+    return patternMatches;
+}
+
+function validatecvv(cvv) {
+    const regEx = /^\d+$/;
     const patternMatches = regEx.test(cvv);
     return patternMatches;
 }
 
-form.addEventListener("submit", validateForm);
-
-function validateEmail(email) {
-    const regEx = /\S+@\S+\.\S+/;
-    const patternMatches = regEx.test(email);
-    return patternMatches;
-}
-
-function validateCardNumber(cardNumber) {
-    const regEx =  /^\d+$/;
-    const patternMatches = regEx.test(cardNumber);
-    return patternMatches;
-}
-
-function validateCvv(cvv) {
-    const regEx =  /^\d+$/;
-    const patternMatches = regEx.test(cvv);
-    return patternMatches;
-}
 
 
 
