a05a8bc4342bfc564f377621d9bb547d22d31eab
Charlotte366 <jc.oren@hotmail.com>
2021-04-21 23:39:22 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const url ="https://rainydays.charlotte366.no/wp-json/wc/store/products/";
+const url ="https://rainydays.charlotte366.no/wp-json/wc/store/products"
 
 const proxy = "https://noroffcors.herokuapp.com/"; 
 
@@ -9,15 +9,14 @@ const resultsContainer = document.querySelector(".results");
 
 
 
-
-async function fetchCategories(){
+async function fetchProducts(){
     
  
         const response = await fetch (corsFix);
 
         const results = await response.json();
 
-        const categories = results.categories;
+        const products = results.products;
 
         resultsContainer.innerHTML ="";
 
@@ -25,12 +24,12 @@ async function fetchCategories(){
 
         let html="";
 
-        for (let i = 0; i < categories.length; i++) {
-            console.log(categories[i].strCategory);
+        for (let i = 0; i < products.length; i++) {
+            console.log(products[i].strCategory);
             html +=  `<div class="category">
-            <h2><a href="details.html?category=${categories[i].strCategory}"> ${categories[i].strCategory} </a></h2>
-                        <p>id:${categories[i].idCategory}</p>
-                        <p>Description: ${categories[i].strCategoryDescription}</p>
+            <h2><a href="details.html?category=${products[i].strCategory}"> ${products[i].strCategory} </a></h2>
+                        <p>id:${products[i].idCategory}</p>
+                        <p>Description: ${products[i].strCategoryDescription}</p>
                         </div>
                         `;
 
@@ -43,4 +42,4 @@ async function fetchCategories(){
  
     }
 
-fetchCategories();
+fetchProducts();
